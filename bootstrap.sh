cfa9b4d409d609cf89c301049503810dc293e540
rubik <rubik@galadriel.localdomain>
2014-07-27 03:19:52 +0800

    Fixed pip installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,5 @@ make shared_lib
 popd
 
 cp submodule/rocksdb/librocksdb.so rocksdb/
-pip install Cython>=0.20 pyrex
+pip install "Cython>=0.20" "pyrex"
 CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH}:`pwd`/submodule/rocksdb/include" LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:`pwd`/submodule/rocksdb" LIBRARY_PATH="${LIBRARY_PATH}:`pwd`/submodule/rocksdb" python setup.py install
